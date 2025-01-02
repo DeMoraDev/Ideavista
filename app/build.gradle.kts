0342354dba351a7486b8ae46acebca7e74223d04
Viejo <demoraserrano20@gmail.com>
2025-01-03 00:55:04 +0100

    Añadido Retrofit- estable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,9 @@ dependencies {
 
     //koin
     implementation(libs.bundles.koin)
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
 }
